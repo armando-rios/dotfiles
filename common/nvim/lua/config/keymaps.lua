18da8b2f326b9272f42d13a11163a7e57c407a6d
Armando Rios <armandorios.dev@proton.me>
2024-10-11 18:51:46 -0700

    change keymaps for file explorer and telescope.

--- common/nvim/lua/config/keymaps.lua
+++ common/nvim/lua/config/keymaps.lua
@@ -13,11 +13,13 @@ keymap("n", "<leader>Q", ":qa!<CR>", { noremap = true, silent = true, desc = "Ex
 -- keymap select option
 keymap("n", "<C-a>", "gg<S-v>G", { desc = "Select all"}) -- select all
 
--- nvim-tree
-keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "File Explorer"})
+-- keymap file explorer
+-- keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "File Explorer"})
+keymap("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open directory"})
 
 -- telescope
 
 local telescope = require('telescope.builtin')
 keymap("n", "<leader>fg", telescope.live_grep, { noremap = true, silent = true, desc = "Live Grep"})
-keymap("n", "<leader>p", telescope.find_files, {noremap= true, silent = true, desc = "Find Files"})
+keymap("n", "<leader>ff", telescope.find_files, { noremap= true, silent = true, desc = "Find Files"})
+keymap("n", "<leader>fn", ":Telescope notify<CR>", { noremap= true, silent = true, desc = "Find Notify"})
