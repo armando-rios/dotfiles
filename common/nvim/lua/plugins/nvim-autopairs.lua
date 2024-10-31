648030e8053ddad89129a3c51ea50b492b21e45c
Armando Rios <armandorios.dev@gmail.com>
2024-10-31 16:28:16 -0700

    update config for nvim-autopairs

--- common/nvim/lua/plugins/nvim-autopairs.lua
+++ common/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,7 +1,22 @@
 return {
     'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
+    event = { "InsertEnter" },
+    dependencies = {
+        "hrsh7th/nvim-cmp",
+    },
+    config = function ()
+        local autopairs = require("nvim-autopairs")
+        autopairs.setup({
+            check_ts = true,
+            ts_config = {
+                lua = { "string" },
+                javascript = { "template_string" },
+            },
+        })
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+        local cmp = require("cmp")
+
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_dode())
+    end
 }
