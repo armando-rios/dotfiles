911367934a0591bb6f65b5080b7c595447b63ebe
Armando Rios <armandorios.dev@gmail.com>
2024-10-31 16:21:10 -0700

    add show hidden files on oil.nvim

--- common/nvim/lua/plugins/file-explorer.lua
+++ common/nvim/lua/plugins/file-explorer.lua
@@ -1,32 +1,36 @@
 return {
-	{
-		"nvim-tree/nvim-tree.lua",
-		proiority = 1000,
-		config = function()
-			require("nvim-tree").setup({
-				sort = {
-					sorter = "case_sensitive",
-				},
-				view = {
-					width = 30,
-					side = "right",
-				},
-				renderer = {
-					group_empty = true,
-				},
-				filters = {
-					dotfiles = true,
-				},
-				actions = {
-					open_file = {
-						quit_on_open = true,
-					},
-				},
-			})
-		end,
-	},
+    {
+        "nvim-tree/nvim-tree.lua",
+        proiority = 1000,
+        config = function()
+            require("nvim-tree").setup({
+                sort = {
+                    sorter = "case_sensitive",
+                },
+                view = {
+                    width = 30,
+                    side = "right",
+                },
+                renderer = {
+                    group_empty = true,
+                },
+                filters = {
+                    dotfiles = true,
+                },
+                actions = {
+                    open_file = {
+                        quit_on_open = true,
+                    },
+                },
+            })
+        end,
+    },
     {
         'stevearc/oil.nvim',
-        opts = {}
+        opts = {
+            view_options = {
+                show_hidden = true
+            }
+        }
     }
 }
