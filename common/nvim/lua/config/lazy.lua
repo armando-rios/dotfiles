8f03b8d58c14339ca220c7a5c016706e07703e52
Armando Rios <armandorios.dev@gmail.com>
2024-10-31 19:43:37 -0700

    Add lsp plugins path

--- common/nvim/lua/config/lazy.lua
+++ common/nvim/lua/config/lazy.lua
@@ -26,10 +26,11 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     { import = "plugins" },
+    { import = "plugins.lsp"}
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
 })
