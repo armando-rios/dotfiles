7f6274a6d1070bfcdbb4e40b6f0aa857d1dedb23
Armando Rios <armandorios.dev@proton.me>
2024-11-05 10:52:38 -0800

    Add new keymaps for telescope

--- common/nvim/lua/config/keymaps.lua
+++ common/nvim/lua/config/keymaps.lua
@@ -8,18 +8,21 @@ keymap("n", "<leader>w", ":w<CR>", { noremap = true, silent = true, desc = "Writ
 keymap("n", "<leader>q", ":bd<CR>", { noremap = true, silent = true, desc = "Close Buffer" })
 
 -- keymap for exit nvim
-keymap("n", "<leader>Q", ":qa!<CR>", { noremap = true, silent = true, desc = "Exit Nvim"})
+keymap("n", "<leader>Q", ":qa!<CR>", { noremap = true, silent = true, desc = "Exit Nvim" })
 
 -- keymap select option
-keymap("n", "<C-a>", "gg<S-v>G", { desc = "Select all"}) -- select all
+keymap("n", "<C-a>", "gg<S-v>G", { desc = "Select all" }) -- select all
 
 -- keymap file explorer
 -- keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "File Explorer"})
-keymap("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open directory"})
+keymap("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open directory" })
 
 -- telescope
 
 local telescope = require('telescope.builtin')
-keymap("n", "<leader>fg", telescope.live_grep, { noremap = true, silent = true, desc = "Live Grep"})
-keymap("n", "<leader>ff", telescope.find_files, { noremap= true, silent = true, desc = "Find Files"})
-keymap("n", "<leader>fn", ":Telescope notify<CR>", { noremap= true, silent = true, desc = "Find Notify"})
+keymap("n", "<leader>lg", telescope.live_grep, { noremap = true, silent = true, desc = "Live Grep" })
+keymap("n", "<leader>ff", telescope.find_files, { noremap = true, silent = true, desc = "Find Files" })
+keymap("n", "<leader>fn", ":Telescope notify<CR>", { noremap = true, silent = true, desc = "Find Notify" })
+keymap("n", "<leader>fb", telescope.buffers, { noremap = true, silent = true, desc = "Find Notify" })
+keymap("n", "<leader>fg", telescope.git_commits, { noremap = true, silent = true, desc = "Find Notify" })
+keymap("n", "<leader>fc", telescope.colorscheme, { noremap = true, silent = true, desc = "Find Notify" })
