ba95cb2df2e13af9771a9e2608a6cddb35fd8d6d
Armando Rios <armandorios.dev@gmail.com>
2024-11-05 11:27:13 -0800

    test: delete transparent nvim

--- common/nvim/lua/plugins/transparent.lua
+++ common/nvim/lua/plugins/transparent.lua
@@ -1,11 +1,11 @@
 return {
-    'xiyaowong/transparent.nvim',
-    config = function ()
-        require('transparent').setup({
-            extra_groups = {
-                "NormalFloat", -- plugins which have float panel such Lazy, Mason, LspInfo
-                "NvimTreeNormal", -- NvimTree
-            },
-        })
-    end,
+    -- 'xiyaowong/transparent.nvim',
+    -- config = function ()
+    --     require('transparent').setup({
+    --         extra_groups = {
+    --             "NormalFloat", -- plugins which have float panel such Lazy, Mason, LspInfo
+    --             "NvimTreeNormal", -- NvimTree
+    --         },
+    --     })
+    -- end,
 }
