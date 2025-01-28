4b40ea3f81bbc048416b09220f4d122c4cfe7166
Armando Rios <armandorios.dev@gmail.com>
2025-01-27 18:40:33 -0800

    feat: disable lazy checker

---
+++ nvim/init.lua
@@ -0,0 +1,30 @@
+-- Neovim started config
+-- Bootstrap lazy.nvim
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none", "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    { import = "plugins" }
+  },
+  -- install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "kanagawa" } },
+  checker = { enabled = true },
+})
+
+-- Load basic configs
+require("config.options")
+require("config.keymaps")
