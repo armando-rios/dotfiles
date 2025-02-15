a8ce4e817c69ccf95db0aed4cc4d57dada660bf5
Armando Rios <armandorios.dev@gmail.com>
2025-02-14 21:19:53 -0800

    feat: add lazy event on plugins

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,12 +1,13 @@
 return {
-  {
-    "github/copilot.vim",
-    config = function()
-      -- Desactivar Copilot por defecto al inicio
-      vim.g.copilot_enabled = 0
-    end
-  },
-  {
-    'Exafunction/codeium.vim',
-  }
+	{
+		"github/copilot.vim",
+		lazy = true,
+		config = function()
+			-- Desactivar Copilot por defecto al inicio
+			vim.g.copilot_enabled = 0
+		end,
+	},
+	{
+		"Exafunction/codeium.vim",
+	},
 }
