21b1c57b9905fd777559ebce58b1f3c758adff8a
Armando Rios <armandorios.dev@gmail.com>
2025-03-19 12:48:04 -0700

    feat: add icons and new cmps plugins

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -3,17 +3,22 @@ return {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
-      "hrsh7th/cmp-buffer", -- source for text in buffer
-      "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
+      "hrsh7th/cmp-nvim-lsp",         -- fuente para LSP
+      "hrsh7th/cmp-buffer",           -- fuente para texto en buffer
+      "hrsh7th/cmp-path",             -- fuente para rutas de archivos
+      "hrsh7th/cmp-cmdline",          -- fuente para línea de comandos
+      "hrsh7th/cmp-nvim-lua",         -- fuente para API de Neovim en Lua
+      "L3MON4D3/LuaSnip",             -- motor de snippets
+      "saadparwaiz1/cmp_luasnip",     -- integración de LuaSnip con cmp
+      "rafamadriz/friendly-snippets", -- colección de snippets
+      "onsails/lspkind.nvim",         -- íconos para el menú de completado
     },
     config = function()
       local cmp = require("cmp")
-
       local luasnip = require("luasnip")
-      -- loads vscode style snippets from installed plugins
+      local lspkind = require("lspkind")
+
+      -- carga snippets de estilo vscode desde plugins instalados
       require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
@@ -25,6 +30,10 @@ return {
             luasnip.lsp_expand(args.body)
           end,
         },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
         mapping = cmp.mapping.preset.insert({
           ["<C-k>"] = cmp.mapping.select_prev_item(),
           ["<C-j>"] = cmp.mapping.select_next_item(),
@@ -32,13 +41,66 @@ return {
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<CR>"] = cmp.mapping.confirm({ select = false }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp", priority = 1000 },
+          { name = "nvim_lua", priority = 900 },
           { name = "luasnip",  priority = 750 },
           { name = "buffer",   priority = 500 },
           { name = "path",     priority = 250 },
         }),
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol_text",
+            maxwidth = 50,
+            ellipsis_char = "...",
+            menu = {
+              nvim_lsp = "[LSP]",
+              nvim_lua = "[Lua]",
+              luasnip = "[Snippet]",
+              buffer = "[Buffer]",
+              path = "[Path]",
+            },
+          }),
+        },
+        experimental = {
+          ghost_text = true, -- muestra texto fantasma para previsualizar
+        },
+      })
+
+      -- Configuración para la línea de comandos
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+          { name = "cmdline" }
+        })
+      })
+
+      -- Configuración para búsqueda
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" }
+        }
       })
     end,
   },
