6a99d837c15940ab9861f0085457cee23ad05f80
Armando Rios <armandorios.dev@gmail.com>
2025-04-05 22:08:54 -0700

    feat: add codecompanion plugin

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -8,6 +8,14 @@ return {
     end,
   },
   {
+    "olimorris/codecompanion.nvim",
+    config = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
+  {
     "Exafunction/codeium.vim",
   },
 }
