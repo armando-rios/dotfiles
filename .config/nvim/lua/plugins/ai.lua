2b06668ef2ae8378eb69b227b891fb92e36ef0bd
Armando Rios <armandorios.dev@gmail.com>
2025-04-05 22:11:21 -0700

    refactor: add copilot as a dependency on codecompanion

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,15 +1,10 @@
 return {
   {
-    "github/copilot.vim",
-    lazy = true,
-    config = function()
-      -- Desactivar Copilot por defecto al inicio
-      vim.g.copilot_enabled = 0
-    end,
-  },
-  {
     "olimorris/codecompanion.nvim",
     config = true,
+    dependencies = {
+      "github/copilot.vim",
+    },
   },
   {
     "Exafunction/codeium.vim",
