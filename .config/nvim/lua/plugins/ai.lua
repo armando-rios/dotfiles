95ca2eb8cf266d930f98a122cafece2e19d3f570
Armando Rios <armandorios.dev@gmail.com>
2025-10-01 20:23:07 -0700

    feat(nvim): add github copilot nvim plugin

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,5 +1,19 @@
+local map = vim.keymap.set
+
 return {
   {
     'Exafunction/windsurf.vim',
   },
+  {
+    'github/copilot.vim',
+    -- functions to enable/disable copilot
+    map("n", "<leader>ce", function()
+      vim.g.copilot_enabled = true
+      vim.notify("Copilot enabled")
+    end, { desc = "Enable Copilot"})
+    map("n", "<leader>cd", function()
+      vim.g.copilot_enabled = false
+      vim.notify("Copilot disabled")
+    end, { desc = "Disable Copilot"})
+  }
 }
